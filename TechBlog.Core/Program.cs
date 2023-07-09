f5be1c24444de4b46772cadee287797c4044eb8c
Phạm Sơn <pson315@gmail.com>
2023-07-09 19:58:20 +0700

    CoreAndUI

---
+++ TechBlog.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TechBlog.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
